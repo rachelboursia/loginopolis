62cbdd5a7243edb9c01236a810324286c24277e7
rachelboursia <rachel.l.boursia@gmail.com>
2023-05-18 12:49:04 -0700

    updates index.js with POST /register and POST /login

--- index.js
+++ index.js
@@ -17,8 +17,38 @@ app.get('/', async (req, res, next) => {
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
 
+app.post('/register', async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({username, password: hashedPassword});
+    res.send(`successfully created user ${username}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post('/login', async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    const user = await User.findOne({where: {username}});
+    if (!user) {
+      res.status(401).send('Invalid username or password');
+      return;
+    } 
+    const passwordMatch = await bcrypt.compare(password, user.password);  
+    if (passwordMatch) {
+      res.send(`successfully logged in user ${username}`);
+    } else {
+      res.status(401).send('Invalid username or password');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
